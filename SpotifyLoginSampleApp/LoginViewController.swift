3ecf8f96c4dddd376f0efc1bfa6ae7c1cd4fd078
Hyunwoo <gus007dn@naver.com>
2023-01-16 17:32:19 +0900

    Second Commit

--- SpotifyLoginSampleApp/LoginViewController.swift
+++ SpotifyLoginSampleApp/LoginViewController.swift
@@ -6,11 +6,13 @@
 //
 
 import UIKit
+import FirebaseAuth
+import GoogleSignIn
 
 class LoginViewController: UIViewController {
    
     @IBOutlet weak var emailLoginButton: UIButton!
-    @IBOutlet weak var googleLoginButton: UIButton!
+    @IBOutlet weak var googleLoginButton: GIDSignInButton!
     @IBOutlet weak var appleLoginButton: UIButton!
     
     override func viewDidLoad() {
@@ -28,10 +30,36 @@ class LoginViewController: UIViewController {
         
         // navigation bar 숨기기
         navigationController?.navigationBar.isHidden = true
+        
+        //Google Sign In
     }
     
-    @IBAction func googleLoginButtonTapped(_ sender: UIButton) {
-        //Firebase 인증
+    @IBAction func googleLoginButtonTapped(_ sender: GIDSignInButton) {
+        
+        GIDSignIn.sharedInstance.signIn(withPresenting: self) { signInResult, error in
+            guard error == nil else { return }
+            guard let signInResult = signInResult else { return }
+            
+            signInResult.user.refreshTokensIfNeeded { user, error in
+                guard error == nil else { return }
+                guard let user = user else { return }
+                
+                guard let idToken = user.idToken?.tokenString else { return }
+                let accessToken = user.accessToken.tokenString
+                let credential = GoogleAuthProvider.credential(withIDToken: idToken, accessToken: accessToken)
+                
+                Auth.auth().signIn(with: credential) { _, _ in
+                    self.showMainViewController()
+                }
+            }
+        }
+    }
+    
+    private func showMainViewController() {
+        let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+        let mainViewController = storyboard.instantiateViewController(identifier: "MainViewController")
+        mainViewController.modalPresentationStyle = .fullScreen
+        navigationController?.show(mainViewController, sender: nil)
     }
     
     @IBAction func appleLoginButtonTapped(_ sender: UIButton) {
